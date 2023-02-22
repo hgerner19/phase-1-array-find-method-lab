ccc065050cedb9f70cc68847330605935bb02018
Holden Gerner <hgerner19@gmail.com>
2023-02-22 16:32:05 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+function superbowlWin(arrays){
+    for(const user of arrays){
+        if(user.result === "W"){
+            return user.year;
+        }
+    }
+}
+console.log(superbowlWin(record));
